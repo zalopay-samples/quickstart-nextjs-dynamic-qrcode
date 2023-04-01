a1641890ed0c5b922f7b615cc18a8624acb78803
Andy Le <anhldbk@gmail.com>
2023-04-01 16:32:12 +0700

    api: refactor

--- pages/api/callback/index.js
+++ pages/api/callback/index.js
@@ -2,44 +2,43 @@ import CryptoJS from "crypto-js";
 import {configZLP} from "../config";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      let result = {};
-      try {
-        let dataStr = req.body.data;
-        let reqMac = req.body.mac;
-
-        let mac = CryptoJS.HmacSHA256(dataStr, configZLP.key2).toString();
-        console.log("mac =", mac);
-
-
-        // kiểm tra callback hợp lệ (đến từ ZaloPay server)
-        if (reqMac !== mac) {
-          // callback không hợp lệ
-          result.return_code = -1;
-          result.return_message = "mac not equal";
-        } else {
-          // thanh toán thành công
-          // merchant cập nhật trạng thái cho đơn hàng
-          let dataJson = JSON.parse(dataStr, configZLP.key2);
-          console.log(`💰  Payment Callback received!`);
-          console.log("✅  Update order's status = success where app_trans_id =", dataJson["app_trans_id"]);
-
-          result.return_code = 1;
-          result.return_message = "success";
-        }
-      } catch (ex) {
-        result.return_code = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
-        result.return_message = ex.message;
-      }
-
-      // thông báo kết quả cho ZaloPay server
-      res.json(result);
-    } catch (err) {
-      res.status(500).json({statusCode: 500, message: err.message});
-    }
-  } else {
+  if (req.method !== 'POST') {    
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
+    return;
   }
+  try {
+    let result = {};
+    try {
+      let dataStr = req.body.data;
+      let reqMac = req.body.mac;
+
+      let mac = CryptoJS.HmacSHA256(dataStr, configZLP.key2).toString();
+      console.log("mac =", mac);
+
+
+      if (reqMac !== mac) {
+        // invalid callback
+        result.return_code = -1;
+        result.return_message = "mac is not equal";
+      } else {
+        // the payment is successful
+        let dataJson = JSON.parse(dataStr, configZLP.key2);
+        console.log(`💰  Payment Callback received!`);
+        console.log("✅  Update order's status = success where app_trans_id =", dataJson["app_trans_id"]);
+
+        result.return_code = 1;
+        result.return_message = "success";
+      }
+    } catch (ex) {
+      result.return_code = 0; // ZaloPay server will callback (at most 3 times)
+      result.return_message = ex.message;
+    }
+
+    // return response to ZaloPay
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({statusCode: 500, message: err.message});
+  }
+} 
 }
